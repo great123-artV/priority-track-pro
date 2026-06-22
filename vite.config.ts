36f55625b80d8359b67d47d651ca7f460a2ec9bb
Noskytech <echinonso609@gmail.com>
2026-06-22 19:31:11 +0100

    Add nitro preset configuration for deployment platforms

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,11 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: {
+    preset: process.env.NETLIFY
+      ? "netlify"
+      : process.env.CF_PAGES
+      ? "cloudflare-pages"
+      : "vercel",
+  },
 });
